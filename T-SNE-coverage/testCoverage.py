ed37a37bac087ab85ee56dfa8cb20e81ec313ca2
Howard19960920 <hower.song@gmail.com>
2018-06-20 12:57:56 +0800

    init T-SNE

---
+++ T-SNE-coverage/testCoverage.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+from sklearn.manifold import TSNE
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+
+audioPath = '../storage/kp3/'
+X = []
+items = sorted(os.listdir(audioPath))
+
+for item in items:
+    X.append(AudioSegment.from_file(audioPath + item).get_array_of_samples())
+# get min len
+minlen = 1e9+10
+for i in X:
+    if len(i) < minlen:
+        minlen = len(i)
+
+for i in range(len(X)):
+    X[i] = X[i][:minlen]
+
+X_embedded = TSNE(n_components=2).fit_transform(X)
+X_embedded = np.array(X_embedded)
+x, y = X_embedded.T
+plt.scatter(x, y)
+plt.show()
